98fe5b9a49060c9c66389f57c09f57bd6cf61996
Artem <artemdembitskiy@MacBook-Air.local>
2023-12-29 15:34:16 +0300

    setup.py version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='ions',  
-     version='0.2.2',
+     version='0.2.3',
      py_modules = ["ions"],
      install_requires = [
                          "ase>=3.22.1",
